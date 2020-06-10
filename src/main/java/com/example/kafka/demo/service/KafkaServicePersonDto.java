efa9c082c3104e9c8aafc51bb255219aca2e67d2
ivan315315 <ivan315315@mail.ru>
2020-06-10 14:04:49 +0300

    1_prod-2_groupBy2_Cons(crutch)

--- src/main/java/com/example/kafka/demo/service/KafkaServicePersonDto.java
+++ src/main/java/com/example/kafka/demo/service/KafkaServicePersonDto.java
@@ -1,7 +1,6 @@
 package com.example.kafka.demo.service;
 
 import com.example.kafka.demo.dto.PersonDto;
-import com.example.kafka.demo.utils.ConstantData;
 import com.example.kafka.demo.utils.WorkUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -12,6 +11,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import static com.example.kafka.demo.utils.ConstantData.*;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
@@ -34,12 +34,43 @@ public class KafkaServicePersonDto{
 
     public void send(PersonDto dto) {
         log.info("producer: {}", WorkUtils.writeValueAsString(objectMapper, dto));
-        kafkaPersonTemplate.send(ConstantData.topic, dto);
+        kafkaPersonTemplate.send(topic, dto);
     }
 
-    @KafkaListener(id = "Starship", topics = {ConstantData.topic}, containerFactory = "singleFactory")
-    public void consume(PersonDto dto) {
-        log.info("consumer: {}", WorkUtils.writeValueAsString(objectMapper, dto));
+    @KafkaListener(id = Listener1, topics = {topic}, containerFactory = "singleFactory")
+    public void consume1(PersonDto dto) {
+        consume_work(null, Listener1, dto);
+    }
+
+    @KafkaListener(id = Listener2, topics = {topic}, containerFactory = "singleFactory")
+    public void consume2(PersonDto dto) {
+        consume_work(null, Listener2, dto);
+    }
+
+    @KafkaListener(id = Listener3, topics = {topic}, containerFactory = "singleFactory", groupId = Group1)
+    public void consume3(PersonDto dto) {
+        consume_work(Group1, Listener3, dto);
+    }
+
+    @KafkaListener(id = Listener4, topics = {topic}, containerFactory = "singleFactory", groupId = Group1)
+    public void consume4(PersonDto dto) {
+        consume_work(Group1, Listener4, dto);
+    }
+
+    @KafkaListener(id = Listener5, topics = {topic}, containerFactory = "singleFactory", groupId = Group2)
+    public void consume5(PersonDto dto) {
+        consume_work(Group2, Listener5, dto);
+    }
+
+    @KafkaListener(id = Listener6, topics = {topic}, containerFactory = "singleFactory", groupId = Group2)
+    public void consume6(PersonDto dto) {
+        consume_work(Group2, Listener6, dto);
+    }
+
+
+
+    private void consume_work(String group, String listener, PersonDto dto) {
+        log.info("Group: {}; listener: {}; consumer: {}", group, listener, WorkUtils.writeValueAsString(objectMapper, dto));
         try {
             PersonDto personDto = objectMapper.readValue(WorkUtils.writeValueAsString(objectMapper, dto), PersonDto.class);
             log.info("object: {}", personDto);
--- src/main/java/com/example/kafka/demo/utils/ConstantData.java
+++ src/main/java/com/example/kafka/demo/utils/ConstantData.java
@@ -2,4 +2,15 @@ package com.example.kafka.demo.utils;
 
 public class ConstantData {
     public static final String topic = "mytopic";
+
+    public static final String Listener1 = "Listener_1";
+    public static final String Listener2 = "Listener_2";
+
+    public static final String Listener3 = "Listener_3";
+    public static final String Listener4 = "Listener_4";
+    public static final String Group1 = "Group_1";
+
+    public static final String Listener5 = "Listener_5";
+    public static final String Listener6 = "Listener_6";
+    public static final String Group2 = "Group_2";
 }
